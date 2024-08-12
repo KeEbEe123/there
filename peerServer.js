04c496660a25e898944dde95b9c743c69ca35386
Keertan Kuppili <90203723+KeEbEe123@users.noreply.github.com>
2024-08-12 22:35:53 +0530

    Update peerServer.js

--- peerServer.js
+++ peerServer.js
@@ -1,14 +1,15 @@
 const { PeerServer } = require("peer");
-
+const port = process.env.PORT || 4000;
 
 const options = {
-  port: 443,
+  port: port,
   proxied: true,
   path: "/myapp",
 };
 
 try {
   const peerServer = PeerServer(options);
+  console.log(`peer server started on ${port}`)
 
   peerServer.on("connection", (client) => {
     console.log("PeerJS client connected:", client.id);
